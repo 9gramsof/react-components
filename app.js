c09aa2a1d3a4472c500115eea965609f2734e4aa
9gramsof <minggui.yactayo@gmail.com>
2022-03-07 12:38:36 -0800

    react-components solo part

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    })
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    )
+  }
+}
+
+
+var GroceryList = (props) => {
+
+  // var onListItemClick = (event) => {
+  //   console.log('this item got clicked');
+  // };
+  return (
+    <ul>
+      {props.groceries.map(item =>
+        <GroceryListItem item={item} />
+        )}
+    </ul>
+  );
+};
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+      <GroceryList groceries={['kale', 'jello']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
